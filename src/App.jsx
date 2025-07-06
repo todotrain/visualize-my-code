b7b5d219a1ee69180cbb07a4b4ec2de18de64679
todotrain <artemishottendorf@gmail.com>
2025-07-05 21:02:33 -0500

    add user input, ability to search for a specific commit, remove some unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Octokit } from 'octokit'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [commits, setCommits] = useState([])
-  let owner = "coreybutler"
-  let repo = "nvm-windows"
+  const [commit, setCommit] = useState("")
+  const [owner, setOwner] = useState("")
+  const [repo, setRepo] = useState("")
+  const [ref, setRef] = useState("")
 
   const getCommits = async (owner = "nodejs", repo = "nvm-node") => {
     const octokit = new Octokit()
@@ -22,28 +21,36 @@ function App() {
     })
 
     setCommits(response.data)
-
   }
 
+  const getCommit = async (
+      owner = "coreybutler", 
+      repo = "nvm-windows", 
+      ref = "9bee6e77e40a422f4e6d10f23ca3ab518f572f7b") => {
+    const octokit = new Octokit()
+
+    const response = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}', {
+      owner: owner,
+      repo: repo,
+      ref: ref,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+    setCommit(JSON.stringify(response, null, 2))
+  }
 
   return (
     <>
+      <h1>Visualize My Code</h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Owner <input name="owner" onChange={(e)=>setOwner(e.target.value)}></input>
+        Repo <input name="repo" onChange={(e)=>setRepo(e.target.value)}></input>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
         <button onClick={() => getCommits(owner, repo)}>
           Fetch Latest Commits
         </button>
@@ -53,13 +60,19 @@ function App() {
             <ul>
               {commits.slice(0, 5).map(commit => (
                 <li key={commit.sha}>
-                  <strong>{commit.commit.author.name}:</strong> {commit.commit.message}
+                  <strong>{commit.commit.author.name}:</strong> {commit.sha} {commit.commit.message}
                 </li>
               ))}
             </ul>
           </div>
         )}
       </div>
+
+      Ref <input name="ref" onChange={(e)=>setRef(e.target.value)}></input>
+      <button onClick={() => getCommit(owner, repo, ref)}>
+        Fetch a commit
+      </button>
+      {commit !== "" && (<pre>{commit}</pre>)}
     </>
   )
 }
